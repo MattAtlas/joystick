414c089c726ec54e6722e6718e8cdfa0be01c77e
MattAtlas <saltattam@gmail.com>
2015-11-30 18:48:05 -0800

    Update setupADC.c

--- setupADC.c
+++ setupADC.c
@@ -5,3 +5,25 @@
 
 int setupADC()
 {
+  int file;
+  int addr = 0x48;
+  char filename[15];
+  
+  sprintf(filename, "/dev/i2c-%d", 3);
+  file = open(filename, O_RDWR);
+  if(file < 0)
+    {
+      perror("Opening i2c device node.\n");
+      return(1);
+    }
+  ioctl(file, I2C_SLAVE, addr);
+  
+  char setup[4] = {
+    0b10010000,       // chip address
+    0b00000001,       // config address
+    0b00000100,       // MSB config bits
+    0b10000011};      // LSB config bits
+  write(file, &setup, 4);
+  close(file);
+  return(0);
+}
