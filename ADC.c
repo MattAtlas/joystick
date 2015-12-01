67aac71389e3e4b13cf64b2fc43e5b6af73de3b9
MattAtlas <saltattam@gmail.com>
2015-11-30 19:42:46 -0800

    Update and rename setupADC.c to ADC.c

--- ADC.c
+++ ADC.c
@@ -3,12 +3,13 @@
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
 
-int setupADC()
+int ADC(int ch)
 {
   int file;
   int addr = 0x48;
   char filename[15];
-  
+  char buffer[2];
+  int value;
   sprintf(filename, "/dev/i2c-%d", 3);
   file = open(filename, O_RDWR);
   if(file < 0)
@@ -17,13 +18,21 @@ int setupADC()
       return(1);
     }
   ioctl(file, I2C_SLAVE, addr);
+  char MSB[4] = {0b01000100, 0b01010100, 0b01100100, 0b01110100};
   
   char setup[4] = {
     0b10010000,       // chip address
     0b00000001,       // config address
-    0b00000100,       // MSB config bits
+    MSB[ch],          // MSB config bits
     0b10000011};      // LSB config bits
   write(file, &setup, 4);
+  
+  usleep(10);
+  read(file, buffer, 2);
+  
   close(file);
-  return(0);
+  value = (buffer[0] | (buffer[1] << 8));
+  
+  return(value);
 }
+
