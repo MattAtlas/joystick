08515d987c4058473d705342b1a1da6aa1434bb0
MattAtlas <saltattam@gmail.com>
2015-12-14 14:11:46 -0800

    Update ADC.c

--- ADC.c
+++ ADC.c
@@ -8,7 +8,7 @@ int ADC(int ch)
   int file;
   int addr = 0x48;
   char filename[15];
-  char buffer[2];
+  uint8_t buffer[2];
   int value;
   sprintf(filename, "/dev/i2c-%d", 3);
   file = open(filename, O_RDWR);
@@ -18,17 +18,27 @@ int ADC(int ch)
       return(1);
     }
   ioctl(file, I2C_SLAVE, addr);
-  char MSB[4] = {0b01000100, 0b01010100, 0b01100100, 0b01110100};
+  uint8_t MSB[4] = {0b01000100, 0b01010100, 0b01100100, 0b01110100};
   
-  char setup[4] = {
+  uint8_t setup[4] = {
     0b10010000,       // chip address
     0b00000001,       // config address
     MSB[ch],          // MSB config bits
     0b10000011};      // LSB config bits
-  write(file, &setup, 4);
-  
-  usleep(10);
-  read(file, buffer, 2);
+   
+  uint8_t setread[3] = {
+    0b10010000,       // chip address /w write bit
+    0b00000000,       // output register
+    0b10010001};      // chip address /w read bit
+    
+  if (write(file, setup, 4)) != 4) {
+    /* ERROR HANDLING: i2c transaction failed */
+  }
+  if (write(file, setread, 3)) != 3) {
+    /* ERROR HANDLING: i2c transaction failed */
+  }
+  if (read(file, buffer, 2)) != 2) {
+    /* ERROR HANDLING: i2c transaction failed */
   
   close(file);
   value = ((buffer[0] << 4) | (buffer[1] >> 4));
