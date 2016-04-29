79bc29e0e4d19cf380885cae33095d598b89cc52
MattAtlas <saltattam@gmail.com>
2016-04-28 19:38:46 -0700

    Update ADC.h

--- ADC.h
+++ ADC.h
@@ -1,7 +1,17 @@
 #ifndef ADC_H_
 #define ADC_H_
 
+typedef struct Joystick{
+	int value[4];
+	int min[4];
+	int mid[4];
+	int max[4];
+}Joystick;
+
 int get_adc_value(int ch);
 
+int get_all_values(Joystick *J);
+
+int initialize_joystick(Joystick *J);
 
 #endif
